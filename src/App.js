665a6275269f96570fba5456b7ed4523aabfe3df
Erik <erik.solveson@gmail.com>
2021-05-30 21:50:48 -0600

    populating characters and added styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { response } from "msw";
-import { set } from "msw/lib/types/context";
+// import { response } from "msw";
+// import { set } from "msw/lib/types/context";
 // import { data } from "msw/lib/types/context";
 import React, { useEffect, useState } from "react";
 import "./App.css";
@@ -9,7 +9,7 @@ import Character from "./components/Character";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState(null);
+  const [characters, setCharacters] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
@@ -19,14 +19,26 @@ const App = () => {
       .get(`https://swapi.dev/api/people`)
       .then((response) => {
         console.log(response.data);
-        return response.data;
+        setCharacters(response.data);
       });
-    setCharacters(newCharacters);
-  });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="characters">
+        {characters &&
+          characters.map((entry) => {
+            return (
+              <Character
+                name={entry.name}
+                height={entry.height}
+                mass={entry.mass}
+                hair_color={entry.hair_color}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,30 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
 
 function Character(props) {
+  const { name, height, mass, hair_color } = props;
+
+  const StyledCharacter = styled.div`
+    width: 50%;
+    background-color: gray;
+    opacity: 0.8;
+    margin: 20px;
+    boarder-radius: 10px;
+    h2 {
+      color: blue;
+    }
+    &:hover {
+      opacity: 1;
+    }
+  `;
   return (
-    <>
-      <h2>Characters</h2>
-    </>
+    <StyledCharacter>
+      <h2>{name}</h2>
+      <p>Height: {height}</p>
+      <p>Mass: {mass}</p>
+      <p>Hair Color: {hair_color}</p>
+    </StyledCharacter>
   );
 }
 
